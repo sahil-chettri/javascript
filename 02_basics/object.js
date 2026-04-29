67937ce4f3125129c63e0f73c57987d626eff4d9
sahil-chettri <sahill071shimla@gmail.com>
2026-04-29 23:52:39 +0530

    Add 02_basics folder

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -9,19 +9,19 @@ const jsuser = {
     lastLoginDays : ["Monday", "Tuesday", "Wednesday"],
  }
 
-// console.log(jsuser["mail"]);   // this is best way to access the value of a key in an object
-// console.log(jsuser["name"]);   
-// console.log(jsuser["location"]);
-// console.log(jsuser["isLoggedIn"]);
-// console.log(jsuser["lastLoginDays"]);
+console.log(jsuser["mail"]);   // this is best way to access the value of a key in an object
+console.log(jsuser["name"]);   
+console.log(jsuser["location"]);
+console.log(jsuser["isLoggedIn"]);
+console.log(jsuser["lastLoginDays"]);
 
-// jsuser.name = "sahil chettri"  // this is the  way to update the value of a key in an object
-// console.log(jsuser);
+jsuser.name = "sahil chettri"  // this is the  way to update the value of a key in an object
+console.log(jsuser);
 
 
-// Object.freeze(jsuser)  // this method is used to freeze the object and prevent any changes to it
-// jsuser.name = "sahil "  // this will not update the name because the object is frozen
-// console.log(jsuser);
+Object.freeze(jsuser)  // this method is used to freeze the object and prevent any changes to it
+jsuser.name = "sahil "  // this will not update the name because the object is frozen
+console.log(jsuser);
 
 
 
